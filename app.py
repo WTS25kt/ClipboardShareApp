2a4a88b841192d1d8c060710574476002773080e
WTS25kt <ishii25kenta@gmail.com>
2024-07-25 10:07:26 +0900

    成功
    Flaskがルートを認識していないことが原因。
    ルートの情報を追加することで表示できるようになった

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import subprocess
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/save_clipboard', methods=['POST'])
 def save_clipboard():
