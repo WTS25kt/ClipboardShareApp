c7553886892285c989494a5307869ba371626d45
WTS25kt <ishii25kenta@gmail.com>
2024-07-25 10:38:18 +0900

    成功07251035　認証回数を減らす
    認証回数を減らす機能を追加

--- bygoogledrive_up.py
+++ bygoogledrive_up.py
@@ -4,7 +4,14 @@ from pydrive2.drive import GoogleDrive
 
 # Google Drive 認証
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
+
+# 保存された認証情報を使用
+gauth.LoadCredentialsFile("credentials.json")
+
+if gauth.credentials is None or gauth.access_token_expired:
+    gauth.LocalWebserverAuth()  # 新しい認証情報を取得
+    gauth.SaveCredentialsFile("credentials.json")  # 認証情報を保存
+
 drive = GoogleDrive(gauth)
 
 def upload_clipboard_content():
